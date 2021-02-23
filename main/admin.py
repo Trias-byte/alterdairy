499fd2524c0d0771468c49c00039e658083ff13a
Trias <admin@trias.com>
2021-02-23 21:01:36 +0300

    Добавил нормальные имена в бд

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Peaple
 
-# Register your models here.
+
+class PeapleAdmin(admin.ModelAdmin):
+    list_display = ('firstName', 'secondName', 'thirdName', 'role', 'classes', 'rayon')
+    list_filter = ('role', 'classes', 'rayon', 'firstName')
+    search_fields = ('firstName',)
+
+
+admin.site.register(Peaple, PeapleAdmin)
--- main/models.py
+++ main/models.py
@@ -4,22 +4,21 @@ from django.db import models
 
 
 class Peaple(models.Model):
-    firstName = models.CharField('Имя', max_length=25)
+    firstName = models.CharField('Фамилия', max_length=25)
     secondName = models.CharField('Имя', max_length=25)
-    thirdName = models.CharField('Имя', max_length=25)
-    classes = models.CharField('Имя', max_length=25)
-    school = models.CharField('Имя', max_length=25)
-    rayon = models.CharField('Имя', max_length=25)
-    login = models.CharField('Имя', max_length=25)
-    role = models.CharField('Имя', max_length=10)
-    password = models.CharField('Имя', max_length=10)
-    if role == 'ученик':
-        passwordParents = models.CharField('Имя', max_length=10)
+    thirdName = models.CharField('Очество', max_length=25)
+    school = models.CharField('Школа', max_length=25)
+    rayon = models.CharField('Район', max_length=25)
+    login = models.CharField('логин', max_length=25)
+    role = models.CharField('Ученик/Учитель', max_length=10)
+    password = models.CharField('пароль', max_length=10)
+    classes = models.CharField('Класс (None в случе отсутствия)', max_length=25)
+    passwordParents = models.CharField('Пароль для родителей (None в случе ненадобности)', max_length=10)
 
     class Meta:
         ordering = ('login',)
-        verbose_name = 'Маршрут'
-        verbose_name_plural = 'Маршруты'
+        verbose_name = 'Человек'
+        verbose_name_plural = 'Люди'
 
     def str(self):
         return self.firstName
